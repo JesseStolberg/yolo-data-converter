c1daa6d920074bb45963604815f6be8abbe16b79
Jesse Stolberg <JesseStolberg@gmail.com>
2025-01-03 00:00:01 -0700

    added json parsing for DocLayNet COCO data

--- hello.go
+++ hello.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"os"
 )
+
+type c struct {
+	SuperCategory string `json:"supercategory"`
+	Id            int    `json:"id"`
+	Name          string `json:"name"`
+}
+
+type i struct {
+	Id          int    `json:"id"`
+	Width       int    `json:"width"`
+	Height      int    `json:"height"`
+	FileName    string `json:"file_name"`
+	Collection  string `json:"collection"`
+	DocName     string `json:"doc_name"`
+	PageNum     int    `json:"page_no"`
+	Precedence  int    `json:"precedence"`
+	DocCategory string `json:"doc_category"`
+}
+
+type a struct {
+	Id         int         `json:"id"`
+	ImageId    int         `json:"image_id"`
+	CategoryId int         `json:"category_id"`
+	Bbox       []float64   `json:"bbox"`
+	Segs       [][]float64 `json:"segmentation"`
+	Area       float64     `json:"area"`
+	Crowd      int         `json:"iscrowd"`
+	Precedence int         `json:"precedence"`
+}
+
+type doc struct {
+	Categories  []c `json:"categories"`
+	Images      []i `json:"images"`
+	Annotations []a `json:"annotations"`
+}
 
 func check(e error) {
 	if e != nil {
@@ -12,7 +47,10 @@ func check(e error) {
 }
 
 func main() {
-	dat, err := os.ReadFile("./hello.txt")
+	dat, err := os.ReadFile("C:/Users/Jesse/PycharmProjects/PythonProject/DocLayNet_core/COCO/test.json")
 	check(err)
-	fmt.Print(string(dat))
+	var data doc
+	err = json.Unmarshal(dat, &data)
+	check(err)
+
 }
