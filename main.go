a2939a2223a8a1c9b2435713d2c69b7bd4fa11fe
Jesse Stolberg <JesseStolberg@gmail.com>
2025-01-03 23:05:05 -0700

    implemented annotation txt file creation

--- main.go
+++ main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const imgSz = 1025
+const filePath = "C:/Users/Jesse/PycharmProjects/PythonProject/DocLayNet_core/COCO/train.json"
 
-type c struct {
+type class struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
-
-type i struct {
+type image struct {
 	Id          int    `json:"id"`
 	FileName    string `json:"file_name"`
-	Annotations []a
+	Annotations []annotation
 }
-
-type a struct {
+type annotation struct {
 	ImageId    int       `json:"image_id"`
 	CategoryId int       `json:"category_id"`
 	Bbox       []float64 `json:"bbox"`
 }
-
 type jdoc struct {
-	Categories  []c `json:"categories"`
-	Images      []i `json:"images"`
-	Annotations []a `json:"annotations"`
+	Categories  []class      `json:"categories"`
+	Images      []image      `json:"images"`
+	Annotations []annotation `json:"annotations"`
 }
 
 func check(e error) {
@@ -35,41 +35,50 @@ func check(e error) {
 		panic(e)
 	}
 }
-func transform(annotation *a) {
-	annotation.Bbox[0] = annotation.Bbox[0] + annotation.Bbox[2]/2
-	annotation.Bbox[1] = annotation.Bbox[1] + annotation.Bbox[3]/2
-	annotation.Bbox[0] /= imgSz
-	annotation.Bbox[1] /= imgSz
-	annotation.Bbox[2] /= imgSz
-	annotation.Bbox[3] /= imgSz
-	annotation.CategoryId -= 1
+func transform(a *annotation) {
+	a.Bbox[0] = a.Bbox[0] + a.Bbox[2]/2
+	a.Bbox[1] = a.Bbox[1] + a.Bbox[3]/2
+	a.Bbox[0] /= imgSz
+	a.Bbox[1] /= imgSz
+	a.Bbox[2] /= imgSz
+	a.Bbox[3] /= imgSz
+	a.CategoryId -= 1
 
 }
-
 func main() {
-	dat, err := os.ReadFile("C:/Users/Jesse/PycharmProjects/PythonProject/DocLayNet_core/COCO/val.json")
+	b, err := os.ReadFile(filePath)
 	check(err)
-	var data jdoc
-	err = json.Unmarshal(dat, &data)
+	var d jdoc
+	err = json.Unmarshal(b, &d)
 	check(err)
-	for idx := range data.Categories {
-		data.Categories[idx].Id -= 1
+	for i := range d.Categories {
+		d.Categories[i].Id -= 1
 	}
 	j := 0
-	for _, annotation := range data.Annotations {
-		for annotation.ImageId > data.Images[j].Id {
+	for _, a := range d.Annotations {
+		for a.ImageId > d.Images[j].Id {
 			j++
 		}
-		if annotation.ImageId != data.Images[j].Id {
+		if a.ImageId != d.Images[j].Id {
 			panic(err)
 		}
-		transform(&annotation)
-		data.Images[j].Annotations = append(data.Images[j].Annotations, annotation)
+		transform(&a)
+		d.Images[j].Annotations = append(d.Images[j].Annotations, a)
 
 	}
-	// TODO: create file structure
-	// creat YAML file
-	// genorate annotation text files
-	// move image files (or symlink to files with new names)
+	err = os.MkdirAll("labels/train", 0777)
+	check(err)
+	err = os.Chdir("labels/train")
+	check(err)
+	for _, i := range d.Images {
+		f, err := os.Create(strings.Split(i.FileName, ".")[0] + ".txt")
+		check(err)
+		defer f.Close()
+		for _, a := range i.Annotations {
 
+			_, err = f.WriteString(strconv.Itoa(a.CategoryId) + " " + strconv.FormatFloat(a.Bbox[0], 'f', 6, 64) + " " + strconv.FormatFloat(a.Bbox[1], 'f', 6, 64) + " " + strconv.FormatFloat(a.Bbox[2], 'f', 6, 64) + " " + strconv.FormatFloat(a.Bbox[3], 'f', 6, 64) + "\n")
+			check(err)
+		}
+		f.Close()
+	}
 }
